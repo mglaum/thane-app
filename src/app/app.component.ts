4d9dff92dd207a49c7b095059cc9e67b4122ad60
mglaum <m.glaum@ufl.edu>
2023-06-01 15:18:10 -0400

    Fixing deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'thane-app';
+  toDisplaypp = false; 
+  toDisplayje = false; 
+  toDisplaypo = false; 
+
+  togglePeepee() {
+    this.toDisplaypp = !this.toDisplaypp; 
+    if (this.toDisplaypp == true) {
+      let meow = new Audio(); 
+      meow.src = "../assets/sounds/meow.mp3"; 
+      meow.load(); 
+      meow.play(); 
+    }
+  }
+  toggleJesse() {
+    this.toDisplayje = !this.toDisplayje; 
+    if (this.toDisplayje == true) {
+      let woof = new Audio(); 
+      woof.src = "../assets/sounds/bark.mp3"; 
+      woof.load(); 
+      woof.play(); 
+  }
+}
+  togglePoop() {
+    this.toDisplaypo = !this.toDisplaypo; 
+    if (this.toDisplaypo == true) {
+      let fart = new Audio(); 
+      fart.src = "../assets/sounds/fart.mp3"; 
+      fart.load(); 
+      fart.play(); 
+  }
+}
 }
